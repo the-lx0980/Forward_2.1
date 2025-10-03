bce5cd625ea51da48171c8a9142a88f7d343aac4
thelx0980 <133576014+thelx0980@users.noreply.github.com>
2025-10-04 00:48:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ class Config:
     APP_ID = int(getenv("APP_ID", "21288218"))
     API_HASH = getenv("API_HASH", "dd47d5c4fbc31534aa764ef9918b3acd")
     TG_BOT_TOKEN = getenv("TG_BOT_TOKEN", "") 
-    OWNER_ID = int(getenv("OWNER_ID", "")
+    OWNER_ID = int(getenv("OWNER_ID", ""))
     DATABASE_URI = getenv("DATABASE_URI", "")
     TO_CHANNEL = int(getenv('TO_CHANNEL'))
