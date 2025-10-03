69bc5d9329720deb8e6f4280a6b517590b7fe494
lx0980 <138602811+lx0980@users.noreply.github.com>
2025-10-04 00:24:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-import os
+import os import getenv
 class Config:   
-    APP_ID = int(os.environ.get("APP_ID", "21288218"))
-    API_HASH = os.environ.get("API_HASH", "dd47d5c4fbc31534aa764ef9918b3acd")
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "") 
-    OWNER_ID = int(os.environ.get("OWNER_ID", "")
-    DATABASE_URI = os.environ.get("DATABASE_URI", "")
-    TO_CHANNEL = int(os.environ.get('TO_CHANNEL'))
+    APP_ID = int(getenv("APP_ID", "21288218"))
+    API_HASH = getenv("API_HASH", "dd47d5c4fbc31534aa764ef9918b3acd")
+    TG_BOT_TOKEN = getenv("TG_BOT_TOKEN", "") 
+    OWNER_ID = int(getenv("OWNER_ID", "")
+    DATABASE_URI = getenv("DATABASE_URI", "")
+    TO_CHANNEL = int(getenv('TO_CHANNEL'))
