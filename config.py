5dd4ad37483a0980727f40415fa8553ee23eaccd
lx0980 <138602811+lx0980@users.noreply.github.com>
2025-10-04 01:45:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ class Config:
     API_ID = int(getenv("API_ID", ""))
     API_HASH = getenv("API_HASH", "")
     TG_BOT_TOKEN = getenv("TG_BOT_TOKEN", "") 
-    OWNER_ID = int(getenv("OWNER_ID", ""))
+    OWNER_ID = set(int(x) for x in getenv("OWNER_ID", "").split())
     DATABASE_URI = getenv("DATABASE_URI", "")
     TO_CHANNEL = int(getenv('TO_CHANNEL'))
